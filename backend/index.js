b928c4161d47a01c2d08d0d36c63edc54ae00c6e
Ankush Dutta <ankush.ad050203@gmail.com>
2022-12-23 22:14:24 +0530

    routes and posts skeleton

---
+++ backend/index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import postRouter from "./routes/posts.js";
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+app.use("/posts", postRouter);
+
+const CONNECTION_URL ="mongodb+srv://Ankush_523:Imperio523@cluster0.q9yepcg.mongodb.net/?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 8000;
+
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`Server Running on Port: http://localhost:${PORT}`)
+    )
+  )
+  .catch((error) => console.log(`${error} did not connect`));
