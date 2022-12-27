059fa345c1ae3bf9c034afec45cde5f02d8204f2
Ankush Dutta <ankush.ad050203@gmail.com>
2022-12-27 11:49:42 +0530

    dotenv added

--- backend/index.js
+++ backend/index.js
@@ -2,21 +2,22 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+import dotenv from "dotenv";
 import postRouter from "./routes/posts.js";
 
 const app = express();
-
+dotenv.config();
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 app.use("/posts", postRouter);
 
-const CONNECTION_URL ="mongodb+srv://Ankush_523:Imperio523@cluster0.q9yepcg.mongodb.net/?retryWrites=true&w=majority";
+// const CONNECTION_URL ="mongodb+srv://Ankush_523:Imperio523@cluster0.q9yepcg.mongodb.net/?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 8000;
 
 mongoose
-  .connect(CONNECTION_URL, {
+  .connect(process.env.CONNECTION_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
