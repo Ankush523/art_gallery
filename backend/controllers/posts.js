6c52ab3f278429460390f4a57fb70ca57091c514
Ankush Dutta <ankush.ad050203@gmail.com>
2022-12-28 11:05:08 +0530

    getPost updated

--- backend/controllers/posts.js
+++ backend/controllers/posts.js
@@ -1,6 +1,10 @@
 import PostMessage from "../models/postMessage.js";
 import mongoose from 'mongoose'
+import express from 'express';
 //each call back has a try and catch block
+
+const router = express.Router();
+
 export const getPosts = async(req,res) => {
     try {
         const postMessages = await PostMessage.find()
@@ -11,21 +15,35 @@ export const getPosts = async(req,res) => {
     }
 };
 
-export const createPosts = async(req,res) => {
-    const post = req.body()
-    const newPost = PostMessage(post)
-    try {
-        await newPost.save()
+export const getPost = async (req, res) => { 
+    const { id } = req.params;
 
-        res.status(201).json(newPost)
+    try {
+        const post = await PostMessage.findById(id);
+        
+        res.status(200).json(post);
     } catch (error) {
-        res.status(409).json({message: error.message})
+        res.status(404).json({ message: error.message });
+    }
+}
+
+export const createPost = async (req, res) => {
+    const { title, message, selectedFile, creator, tags } = req.body;
+
+    const newPostMessage = new PostMessage({ title, message, selectedFile, creator, tags })
+
+    try {
+        await newPostMessage.save();
+
+        res.status(201).json(newPostMessage );
+    } catch (error) {
+        res.status(409).json({ message: error.message });
     }
 }
 
 export const updatePost = async(req,res) => {
     const {id: _id} = req.params
-    const post= req.body
+    const { title, message, creator, selectedFile, tags } = req.body;
     if(!mongoose.Types.ObjectId.isValid(_id))
         return res.status(404).send('No post with this id')
     const updatedPost = await PostMessage.findByIdAndUpdate(_id,{...post,_id}, {new : true})
@@ -53,3 +71,5 @@ export const likePost = async(req,res) => {
     const updatedPost = await PostMessage.findByIdAndUpdate(_id, {likeCount: post.likeCount+1}, {new: true})
     res.json(updatePost)
 }
+
+export default router
--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,10 +1,12 @@
 import express from 'express'
-import { createPosts, getPosts,updatePost,deletePost,likePost } from '../controllers/posts.js';
+import { createPost,getPosts,getPost,updatePost,deletePost,likePost } from '../controllers/posts.js';
 const router = express.Router()
 
-router.get('/', getPosts)
-router.post('/',createPosts)
-router.patch('/:id',updatePost)
-router.delete('/:id',deletePost)
-router.patch('/:id/likePost',likePost)
+router.get('/', getPosts);
+router.post('/', createPost);
+router.get('/:id', getPost);
+router.patch('/:id', updatePost);
+router.delete('/:id', deletePost);
+router.patch('/:id/likePost', likePost);
+
 export default router;
